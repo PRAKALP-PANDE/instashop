399e1132bbe7486e4c6f8247f46d706da61ad26e
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-12-30 18:39:18 +0530

    logic for addToCart

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,66 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const [cart, setCart] = useState({})
+  const [subTotal, setSubTotal] = useState(0)
+  useEffect(() => {
+    console.log("Hey, I am a useEffect from _app.js")
+    try {
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")))
+      }
+    } catch (error) {
+      console.error(error);
+      localStorage.clear()
+    }
+
+  }, [])
+
+
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", myCart)
+    let subt = 0;
+    let keys = Object.keys(cart)
+    for (let i = 0; keys.length; i++) {
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty;
+    }
+    setSubTotal(subt)
+  }
+
+  const addToCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty
+    }
+    else {
+      newCart[itemCode] = { qty: 1, price, name, size, variant }
+    }
+    setCart(newCart)
+    saveCart(newCart)
+  }
+
+  const clearCart = () => {
+    setCart([])
+    saveCart([])
+  }
+
+  const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty
+    }
+    if (newCart[itemCode]["qty"] <= 0) {
+      delete newCart[itemCode]
+    }
+    setCart(newCart)
+    saveCart(newCart)
+  }
   return <>
-    <Navbar />
-    <Component {...pageProps} />
+    <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} />
+    <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
     <Footer />
   </>
 }
