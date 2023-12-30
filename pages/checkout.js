60e34a7cc213a1b21f97eb89d70e69cccc73a855
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-12-31 01:49:48 +0530

    Orders UI

--- pages/checkout.js
+++ pages/checkout.js
@@ -12,20 +12,20 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
       </div>
       <div className="px-2 w-full">
         <div className="mb-4">
           <label htmlFor="email" className="leading-7 text-sm text-gray-600">Address</label>
-          <textarea name="address" id="address" cols="30" rows="2" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+          <textarea name="address" id="address" cols="30" rows="2" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
         </div>
       </div>
 
@@ -33,13 +33,13 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
-            <input type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
-            <input type="text" id="city" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" id="city" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
       </div>
@@ -47,13 +47,13 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="state" className="leading-7 text-sm text-gray-600">State</label>
-            <input type="text" id="state" name="state" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" id="state" name="state" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">PinCode</label>
-            <input type="text" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
       </div>
--- pages/order.js
+++ pages/order.js
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Order = () => {
   return (
-    <div>Order</div>
+    <section className="text-gray-600 body-font overflow-hidden">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+            <h2 className="text-sm title-font text-gray-500 tracking-widest">PRAKALP PANDE</h2>
+            <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">Order Id: #160702</h1>
+            <p className="leading-relaxed mb-4">Your order has been successfully placed.</p>
+            <div className="flex mb-4">
+              <a className="flex-grow text-center py-2 text-lg px-1">Item Description</a>
+              <a className="flex-grow text-center border-gray-300 py-2 text-lg px-1">Quantity</a>
+              <a className="flex-grow text-center border-gray-300 py-2 text-lg px-1">Item Total</a>
+            </div>
+            <div className="flex border-t border-gray-200 py-2">
+              <span className="text-gray-500">Tshirt Blue</span>
+              <span className="ml-auto text-gray-900">1</span>
+              <span className="ml-auto text-gray-900">₹499</span>
+            </div>
+            <div className="flex border-t border-gray-200 py-2">
+              <span className="text-gray-500">Tshirt Red</span>
+              <span className="ml-auto text-gray-900">1</span>
+              <span className="ml-auto text-gray-900">₹499</span>
+            </div>
+            <div className="flex border-t border-b mb-6 border-gray-200 py-2">
+              <span className="text-gray-500">Tshirt White</span>
+              <span className="ml-auto text-gray-900">1</span>
+              <span className="ml-auto text-gray-900">₹499</span>
+            </div>
+            <div className="flex flex-col">
+              <span className="title-font font-medium text-2xl text-gray-900">SubTotal: ₹58.00</span>
+              <div className="my-6">
+                <button className="flex mx-0 text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">Track Order</button>
+              </div>
+            </div>
+          </div>
+          <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
+        </div>
+      </div>
+    </section>
   )
 }
 
