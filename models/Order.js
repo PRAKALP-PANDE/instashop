2232feded6dcba3359ce3dfb91847323863d09c2
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-12-31 14:15:11 +0530

    Api for orders

---
+++ models/Order.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    products: [{
+        productId: { type: String },
+        quantity: { type: Number, default: 1 }
+    }],
+    address: { type: String, required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, default: 'Pending', required: true },
+}, { timestamps: true });
+
+mongoose.models = {}
+export default mongoose.model("Order", OrderSchema);
