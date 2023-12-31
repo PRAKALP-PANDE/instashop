a565a0e117d87074a249d8fdd43e8e02628ac3d4
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-12-31 14:15:26 +0530

    Api for Products

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    slug: { type: String, unique: true, required: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    category: { type: String, required: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+    availableQty: { type: Number, required: true },
+
+}, { timestamps: true });
+
+mongoose.models = {}
+export default mongoose.model("Product", ProductSchema);
