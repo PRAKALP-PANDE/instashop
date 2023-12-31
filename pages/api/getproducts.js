674ce3c84b1c5eeb8cc14e14dd1792aaa7653765
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-12-31 14:16:17 +0530

    API call : Get Product

---
+++ pages/api/getproducts.js
@@ -0,0 +1,8 @@
+import Product from "../../models/Product"
+import connectDb from "../../middleware/mongoose"
+
+const handler = async (req, res) => {
+    let products = await Product.find()
+    res.status(200).json({ products })
+}
+export default connectDb(handler);
