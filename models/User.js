aeaf9203d415921f2ec04f355460aa2e3d8c1a47
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-12-31 14:15:40 +0530

    Api for Users

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    
+}, { timestamps: true });
+mongoose.models = {}
+export default mongoose.model("User", UserSchema);
