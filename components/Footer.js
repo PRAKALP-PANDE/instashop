48d54617a989618d686de188cb19112f0178d4cc
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-01-03 00:51:01 +0530

    Removed Dummy text

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                 <Image src="/logo.png" alt='logo' width={200} height={40} />
               {/* </a> */}
             </Link>
-            <p className="mt-2 text-sm text-gray-500 px-4">Air plant banjo lyft occupy retro adaptogen indego</p>
+            <p className="mt-2 text-sm text-gray-500 px-4">Wear the Code</p>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
@@ -88,8 +88,8 @@ const Footer = () => {
         </div>
         <div className="bg-gray-100">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-              <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+            <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Prakalp Pande — All Right Reserved
+              <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@prakalp-pande</a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
               <a className="text-gray-500">
