71cd72777f9ad432f16b7f30e06d26f442ae0ce3
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-01-08 01:35:57 +0530

    Shopping Cart fix

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className='overflow-x-hidden' lang="en">
       <Head />
       <body>
         <Main />
