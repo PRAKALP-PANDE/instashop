0d73a7de5719c1eadc2938cd06302d7990620437
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-01-09 21:26:12 +0530

    Added environment variable for JWT and AES

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -5,9 +5,8 @@ var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
     if (req.method == 'POST') {
-        console.log(req.body)
         const { name, email } = req.body
-        let u = new User({ name, email, password: CryptoJS.AES.encrypt(req.body.password, 'secret123').toString() })
+        let u = new User({ name, email, password: CryptoJS.AES.encrypt(req.body.password, process.env.AES_SECRET).toString() })
         u.save()
 
         res.status(200).json({ success: "success" })
