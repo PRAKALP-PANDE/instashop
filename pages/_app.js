db52f3db3c3710d90e1c2c3c543d8e19ded67f90
PRAKALP-PANDE <prakalpsp16@gmail.com>
2024-01-10 13:03:12 +0530

    Hosting

--- pages/_app.js
+++ pages/_app.js
@@ -68,7 +68,8 @@ export default function App({ Component, pageProps }) {
   }
 
   const buyNow = (itemCode, qty, price, name, size, variant) => {
-    let newCart = { itemCode: { qty: 1, price, name, size, variant } }
+    let newCart = {}
+    newCart[itemCode] = { qty: 1, price, name, size, variant }
     setCart(newCart)
     saveCart(newCart)
     router.push('/checkout')
--- pages/order.js
+++ pages/order.js
@@ -1,36 +1,36 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import Order from '@/models/Order'
+import mongoose from 'mongoose'
 
-const Order = () => {
+const MyOrder = ({ order }) => {
+  const products = order.products;
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
         <div className="lg:w-4/5 mx-auto flex flex-wrap">
           <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">PRAKALP PANDE</h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">Order Id: #160702</h1>
-            <p className="leading-relaxed mb-4">Your order has been successfully placed.</p>
+            <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">Order Id: #{order.orderId}</h1>
+            <p className="leading-relaxed mb-4">Your order has been successfully placed. Your Payment Status is {order.status}</p>
             <div className="flex mb-4">
               <a className="flex-grow text-center py-2 text-lg px-1">Item Description</a>
               <a className="flex-grow text-center border-gray-300 py-2 text-lg px-1">Quantity</a>
               <a className="flex-grow text-center border-gray-300 py-2 text-lg px-1">Item Total</a>
             </div>
-            <div className="flex border-t border-gray-200 py-2">
-              <span className="text-gray-500">Tshirt Blue</span>
-              <span className="ml-auto text-gray-900">1</span>
-              <span className="ml-auto text-gray-900">₹499</span>
-            </div>
-            <div className="flex border-t border-gray-200 py-2">
-              <span className="text-gray-500">Tshirt Red</span>
-              <span className="ml-auto text-gray-900">1</span>
-              <span className="ml-auto text-gray-900">₹499</span>
-            </div>
-            <div className="flex border-t border-b mb-6 border-gray-200 py-2">
-              <span className="text-gray-500">Tshirt White</span>
-              <span className="ml-auto text-gray-900">1</span>
-              <span className="ml-auto text-gray-900">₹499</span>
-            </div>
+
+            {Object.keys(products).map((key) => {
+
+              return <div key={key} className="flex border-t border-gray-200 py-2">
+                <span className="text-gray-500">{products[key].name}({products[key].size}/{products[key].variant})</span>
+                <span className="m-auto text-gray-900">{products[key].qty}</span>
+                <span className="m-auto text-gray-900">₹{products[key].price}</span>
+              </div>
+            })}
+
+
             <div className="flex flex-col">
-              <span className="title-font font-medium text-2xl text-gray-900">SubTotal: ₹58.00</span>
+              <span className="title-font font-medium text-2xl text-gray-900">SubTotal: ₹{order.amount}</span>
               <div className="my-6">
                 <button className="flex mx-0 text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">Track Order</button>
               </div>
@@ -43,4 +43,15 @@ const Order = () => {
   )
 }
 
-export default Order
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI)
+  }
+  let order = await Order.findById(context.query.id)
+
+  return {
+    props: { order: JSON.parse(JSON.stringify(order)) }, // will be passes to the page component as props
+  }
+}
+
+export default MyOrder
